a86d8be326545d61c097bb8d9b11dec7f63a3412
AdvancedHMI <support@advancedhmi.com>
2017-07-15 10:11:03 -0400

    Add files via upload

---
+++ EthernetIPforCLXCom/WriteUDT/DateTimeType/DateTimeType.vb
@@ -0,0 +1,9 @@
+﻿Public Structure DateTimeType
+    Dim Year As Int32
+    Dim Month As Int32
+    Dim Day As Int32
+    Dim Hour As Int32
+    Dim Minute As Int32
+    Dim Second As Int32
+    Dim Microseconds As Int32
+End Structure
